4ef2fe1e91fb9151aea26f00a16be33c6fa90bc7
SamiCaceres <samicaceres_10x@hotmail.com>
2021-04-02 17:33:34 -0500

    se agrega los metodos

--- POO_Hospital/src/models/Doctor.java
+++ POO_Hospital/src/models/Doctor.java
@@ -7,4 +7,62 @@ public class Doctor {
     private String Especialidad;
     private String Turno;
     private String Telefono;
+
+    public Doctor(int Cod_Doctor, String Nombre_Doctor, String Apellido_Doctor, String Especialidad, String Turno, String Telefono) {
+        this.Cod_Doctor = Cod_Doctor;
+        this.Nombre_Doctor = Nombre_Doctor;
+        this.Apellido_Doctor = Apellido_Doctor;
+        this.Especialidad = Especialidad;
+        this.Turno = Turno;
+        this.Telefono = Telefono;
+    }
+
+    public int getCod_Doctor() {
+        return Cod_Doctor;
+    }
+
+    public void setCod_Doctor(int Cod_Doctor) {
+        this.Cod_Doctor = Cod_Doctor;
+    }
+
+    public String getNombre_Doctor() {
+        return Nombre_Doctor;
+    }
+
+    public void setNombre_Doctor(String Nombre_Doctor) {
+        this.Nombre_Doctor = Nombre_Doctor;
+    }
+
+    public String getApellido_Doctor() {
+        return Apellido_Doctor;
+    }
+
+    public void setApellido_Doctor(String Apellido_Doctor) {
+        this.Apellido_Doctor = Apellido_Doctor;
+    }
+
+    public String getEspecialidad() {
+        return Especialidad;
+    }
+
+    public void setEspecialidad(String Especialidad) {
+        this.Especialidad = Especialidad;
+    }
+
+    public String getTurno() {
+        return Turno;
+    }
+
+    public void setTurno(String Turno) {
+        this.Turno = Turno;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+    
 }
--- POO_Hospital/src/models/Paciente.java
+++ POO_Hospital/src/models/Paciente.java
@@ -10,5 +10,88 @@ package models;
  * @author samic
  */
 public class Paciente {
+    private int Cod_Paciente;
+    private String DNI;
+    private String Nombre;
+    private String Apellido;
+    private int Edad;
+    private String Sexo;
+    private String Direccion;
+    private String Telefono;
+
+    public Paciente(int Cod_Paciente, String DNI, String Nombre, String Apellido, int Edad, String Sexo, String Direccion, String Telefono) {
+        this.Cod_Paciente = Cod_Paciente;
+        this.DNI = DNI;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Edad = Edad;
+        this.Sexo = Sexo;
+        this.Direccion = Direccion;
+        this.Telefono = Telefono;
+    }
+
+    public int getCod_Paciente() {
+        return Cod_Paciente;
+    }
+
+    public void setCod_Paciente(int Cod_Paciente) {
+        this.Cod_Paciente = Cod_Paciente;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
     
 }
